8f368c13817c0d2e6b357e070f9e5955e62db4bd
Tristan Myers <tristifano@gmail.com>
2019-08-03 20:11:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,16 +10,14 @@ def using_pop (array)
   array.pop
 end
 
-def pop_with_args (array, number)
+def pop_with_args (array, number = 2)
   number.times do
     array.pop
   end
-  array
 end
 
-def shift_with_args (array, number)
+def shift_with_args (array, number = 2)
   number.times do 
     array.pop
   end
-  array
 end
